70228dfec87cbefd09ab4f8f95ea8e027d6b5a6c
Nikolas Dushenko <taurossilvers@gmail.com>
2025-02-06 19:10:54 +0200

    #1.0.1 add cors for localhost

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const cors = require("cors");
 const express = require("express");
 const userRouter = require("./routes/user.routes");
 const eventRouuter = require("./routes/event.routes");
@@ -5,6 +6,7 @@ const eventRouuter = require("./routes/event.routes");
 const PORT = process.env.PORT || 8080;
 
 const app = express();
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 
 app.use(express.json());
 
